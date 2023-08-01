ebe47ca3d3bc7a74916a95536e1a80b76aa0c289
cjfarina <cjfarina@gmail.com>
2023-08-01 11:04:44 -0300

    fix typescript-eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 const prettierConfig = require('./.prettierrc');
 
 module.exports = {
+  //I added @typescript-eslint in order to fix
+  //Error: Definition for rule '@typescript-eslint/no-explicit-any' was not found.  @typescript-eslint/no-explicit-any
+  parser: '@typescript-eslint/parser',
   extends: ['next/core-web-vitals', 'prettier'],
-  plugins: ['prettier', 'simple-import-sort'],
+  plugins: ['prettier', 'simple-import-sort', '@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'error',
     'import/no-anonymous-default-export': 'off',
