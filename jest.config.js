6b4501ea00749b0f679531c872f376908b11908d
cjfarina <cjfarina@gmail.com>
2023-08-02 17:08:17 -0300

    remove .history from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,13 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  modulePathIgnorePatterns: ["<rootDir>/.history/"],
   moduleNameMapper: {
     '^@/(.*)': '<rootDir>/src/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
+
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
