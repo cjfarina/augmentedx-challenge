9393a04e60ef956abba0ac6892e8884f8ddcb704
cjfarina <cjfarina@gmail.com>
2023-08-02 17:44:55 -0300

    Sort button for table

---
+++ src/components/SortButton.tsx
@@ -0,0 +1,26 @@
+import { SortKeys, SortOrder } from '@/types';
+import { MouseEventHandler } from 'react';
+
+function SortButton({
+  sortOrder,
+  columnKey,
+  sortKey,
+  onClick,
+}: {
+  sortOrder: SortOrder;
+  columnKey: SortKeys;
+  sortKey: SortKeys;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`cursor-pointer transition-transform ease-in-out bg-blue-200 hover:bg-blue-700 text-black font-bold pb-1 px-1 ml-2 rounded-full
+      ${sortKey === columnKey && sortOrder === 'desc' ? 'transform rotate-180' : ''}`}
+    >
+      ▲
+    </button>
+  );
+}
+
+export default SortButton;
