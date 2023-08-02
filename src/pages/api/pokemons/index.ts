5f790a1a4a17883ef01395538c0b4022cd92c16a
cjfarina <cjfarina@gmail.com>
2023-08-02 18:01:57 -0300

    add sortKey and sortOrder params to api

--- src/pages/api/pokemons/index.ts
+++ src/pages/api/pokemons/index.ts
@@ -1,12 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import pokemonData from '@/pokemon.json';
+import { SortKeys, SortOrder } from '@/types';
 import { filterPokemons } from '@/utils/apiHelpers';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const filterQueryName = req.query.name ? new RegExp(String(req.query.name), 'i') : /.*/;
+  const sortKey = req.query['sort-key'] ? (req.query['sort-key'] as SortKeys) : null;
+  const sortOrder = req.query['sort-order'] ? (req.query['sort-order'] as SortOrder) : null;
 
-  const filteredPokemons = filterPokemons(pokemonData, filterQueryName);
+  const filteredPokemons = filterPokemons(pokemonData, filterQueryName, sortKey, sortOrder);
 
   res.status(200).json(filteredPokemons);
 };
